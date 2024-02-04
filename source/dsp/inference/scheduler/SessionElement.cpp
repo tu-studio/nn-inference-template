05a26f20d7474d1b190975bf7e0d5fd83dd47cc4
Valentin Ackva <valentin.ackva@insone.de>
2024-02-04 18:12:59 +0100

    moved source file to external lib

--- source/dsp/inference/scheduler/SessionElement.cpp
+++
@@ -1,8 +0,0 @@
-#include "SessionElement.h"
-
-SessionElement::SessionElement(int newSessionID, PrePostProcessor& ppP, InferenceConfig& config) :
-    sessionID(newSessionID),
-    prePostProcessor(ppP),
-    inferenceConfig(config)
-{
-}
